8925f6cecd67afd6e048e81ef48864dbd6903013
moderation <michael@sooper.org>
2011-05-18 10:25:57 -0700

    Added HTTPS profile and profile background entries - see https://twitter.com/#!/twitterapi/status/70255073141329920

--- types.go
+++ types.go
@@ -27,35 +27,38 @@ type Entities struct {
 }
 
 type User struct {
-    Lang                         string
-    Verified                     bool
-    Followers_count              int
-    Location                     string
-    Screen_name                  string
-    Following                    bool
-    Friends_count                int
-    Profile_background_color     string
-    Favourites_count             int
-    Description                  string
-    Notifications                string
-    Profile_text_color           string
-    Url                          string
-    Time_zone                    string
-    Statuses_count               int
-    Profile_link_color           string
-    Geo_enabled                  bool
-    Profile_background_image_url string
-    Protected                    bool
-    Contributors_enabled         bool
-    Profile_sidebar_fill_color   string
-    Name                         string
-    Profile_background_tile      string
-    Created_at                   string
-    Profile_image_url            string
-    Id                           int64
-    Id_str                       string
-    Utc_offset                   int
-    Profile_sidebar_border_color string
+    Lang                               string
+    Verified                           bool
+    Followers_count                    int
+    Location                           string
+    Screen_name                        string
+    Following                          bool
+    Friends_count                      int
+    Profile_background_color           string
+    Favourites_count                   int
+    Description                        string
+    Notifications                      string
+    Profile_text_color                 string
+    Url                                string
+    Time_zone                          string
+    Statuses_count                     int
+    Profile_link_color                 string
+    Geo_enabled                        bool
+    Profile_background_image_url       string
+    Protected                          bool
+    Contributors_enabled               bool
+    Profile_sidebar_fill_color         string
+    Name                               string
+    Profile_background_tile            string
+    Created_at                         string
+    Profile_image_url                  string
+    Id                                 int64
+    Id_str                             string
+    Utc_offset                         int
+    Profile_sidebar_border_color       string
+    Profile_image_url_https            string
+    Profile_background_image_url_https string
+    
 }
 
 type Tweet struct {
